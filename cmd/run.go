e0dc1d2e5cef8276441ff4d8fe35e963c32cdd18
Redstoyn33 <36273864+redstoyn33@users.noreply.github.com>
2024-11-24 23:11:07 +0300

    add full runner

--- cmd/run.go
+++ cmd/run.go
@@ -7,6 +7,8 @@ import (
 	"github.com/budka-tech/envo"
 	"github.com/budka-tech/logit-go"
 	"s3n/internal/config"
+	"s3n/internal/db"
+	"s3n/internal/db/repository"
 	"s3n/internal/endpoint"
 	"s3n/internal/image_processing"
 	"s3n/internal/s3"
@@ -25,15 +27,15 @@ func main() {
 	ctx := logger.NewCtx(context.Background(), op, nil)
 	logger.Info(ctx, "Приложение успешно запущено")
 
-	//pool, err := db.NewClient(ctx, &cfg.DB)
-	//if err != nil {
-	//	logger.Fatal(ctx, fmt.Errorf("ошибка при подключении БД: %s", err))
-	//	panic(err)
-	//}
-	//logger.Info(ctx, "БД сервис успешно запущен")
-	//
-	//dbService := db.NewDBService(repository.NewPostgresRepository(pool))
-	//_ = dbService
+	pool, err := db.NewClient(ctx, &cfg.DB)
+	if err != nil {
+		logger.Fatal(ctx, fmt.Errorf("ошибка при подключении БД: %s", err))
+		panic(err)
+	}
+	logger.Info(ctx, "БД сервис успешно запущен")
+
+	dbService := db.NewDBService(repository.NewPostgresRepository(pool))
+	_ = dbService
 
 	s3Service, err := s3.NewS3Service(ctx, logger, &cfg.S3Service)
 	if err != nil {
@@ -46,14 +48,24 @@ func main() {
 	imageService := image_processing.NewImageService(&cfg.ImageProcessing, logger)
 	_ = imageService
 
-	//endpointService, err := endpoint.NewEndpoint(ctx, s3Service, dbService, imageService, logger)
+	endpointService, err := endpoint.NewEndpoint(ctx, s3Service, dbService, imageService, logger)
+	grpcServer := endpoint.NewGrpcServer(endpointService, logger)
+	logger.Info(ctx, "grpc сервер успешно запущен")
+
+	go func() {
+		err := grpcServer.Run(ctx)
+		if err != nil {
+			logger.Fatal(ctx, fmt.Errorf("ошибка при работе grpc: %s", err))
+			panic(err)
+		}
+	}()
 
 	server := endpoint.NewRedirectServer(s3Service, &cfg.HttpRedirect, logger)
 
 	logger.Info(ctx, "redirect сервер запущен")
 	err = server.Run(ctx)
 	if err != nil {
-		logger.Fatal(ctx, fmt.Errorf("ошибка при запуске redirect сервера: %s", err))
+		logger.Fatal(ctx, fmt.Errorf("ошибка при работе redirect сервера: %s", err))
 		panic(err)
 	}
 }
